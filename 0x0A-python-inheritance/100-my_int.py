2058da30446f6ee0b63aac718b7aafba8d88536e
mohamed khaled <123021751+Kavloa@users.noreply.github.com>
2023-07-11 01:26:00 +0300

    Update 100-my_int.py

--- 0x0A-python-inheritance/100-my_int.py
+++ 0x0A-python-inheritance/100-my_int.py
@@ -1,14 +1,9 @@
 #!/usr/bin/python3
-"""Defines a Rectangle subclass Square"""
-Rectangle = __import__('9-rectangle').Rectangle
+class MyInt(int):
+    def __eq__(self, other):
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        return super().__eq__(other)
 
 
-class Square(Rectangle):
-    """Represent a square"""
-
-    def __init__(self, size):
-        """Initialize a new square"""
-
-        self.integer_validator("size", size)
-        super().__init__(size, size)
-        self.__size = size
