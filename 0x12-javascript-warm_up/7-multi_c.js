da60b09db213b2f77f87b82d8f4f6dc908f082e1
mohamed khaled <123021751+Kavloa@users.noreply.github.com>
2023-09-28 13:36:10 +0300

    Update 7-multi_c.js

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -3,11 +3,6 @@ const x = Math.floor(Number(process.argv[2]));
 if (isNaN(x)) {
   console.log('Missing number of occurrences');
 } else {
-#!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
-  console.log('Missing number of occurrences');
-} else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
